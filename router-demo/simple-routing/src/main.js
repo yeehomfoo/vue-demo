ddaf148e26224da4728d48a7c6af0083c77e5c0e
Yeehom Foo <yeehom147@gmail.com>
2018-09-21 08:30:30 +0800

    add simple routing demo

---
+++ router-demo/simple-routing/src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import routes from './routes'
+import View404 from './pages/404'
+
+/* eslint-disable no-new */
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView || View404
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
---
+++ router-demo/simple-routing/src/routes.js
@@ -0,0 +1,7 @@
+import Home from './pages/Home'
+import About from './pages/About'
+
+export default {
+  '/': Home,
+  '/about': About
+}
